846f87691e1f1da52f6817e9389d6e5c3b2b8954
Ryan Bas <krados@gmail.com>
2017-12-02 00:00:54 -0500

    Some rearranging

--- src/main/scala/advent/day1/Captcha.scala
+++ src/main/scala/advent/day1/Captcha.scala
@@ -6,16 +6,11 @@ import common.Utils.fileLines
 
 
 object Captcha extends App {
+  // Original solution for part 1 that didn't work at all for part 2
   private def captcha(init: List[(Int, Int)])(pair: (List[(Int, Int)], Int) => List[(Int, Int)])(seq: Seq[Int]): Int = {
     def check(p: (Int, Int)) = p._1 == p._2
     seq.foldLeft(init)(pair).map(p => if (check(p)) p._1 else 0).sum
   }
-
-  def captcha(input: String, ahead: Int = 1): Int = {
-    captcha(ahead)(input.toCharArray.map(_.asDigit).toList)
-  }
-
-  def captchaHalf(input: String) = captcha(input, input.length / 2)
 
   def captcha(seq: Seq[Int]): Int = seq match {
     case Nil => 0
@@ -27,8 +22,6 @@ object Captcha extends App {
       captcha(init)(pair)(c.tail.tail)
   }
 
-  def captcha1(list: List[Int]): Int = captcha(1)(list)
-
   // The final solution -- a generalized version for any number of elements ahead
   def captcha(ahead: Int)(list: List[Int]): Int = {
     def elementsEqual(t: (Int, Int)) = t._1 == t._2
@@ -36,6 +29,11 @@ object Captcha extends App {
     list.zip(pairs).map(p => if (elementsEqual(p)) p._1 else 0).sum
   }
 
+  def captcha(input: String, ahead: Int = 1): Int = {
+    captcha(ahead)(input.toCharArray.map(_.asDigit).toList)
+  }
+
+  def captchaHalf(input: String): Int = captcha(input, input.length / 2)
 
   fileLines("day1", "input.txt").forEach(line => {
     println(captchaHalf(line))
